2e68437b6af62c264a9e3e4e2a75878f98a11bf8
Turk Smith <snekeyturtle@gmail.com>
2026-06-06 12:06:27 -0700

    basic server and client logic

--- server.py
+++ server.py
@@ -2,12 +2,74 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-@app.route("/goodbye")
-def goodbye_world():
-    return "<p>Goodbye, World!</p>"
+messages = [{
+        "username":"User",
+        "message":"Hello world!!",
+    
+
+        
+    },{
+        "username":"User2",
+        "message":"Hello mars!!",
+    }]
+
+@app.route("/messages")
+def messages():
+    print("hello")
+    return messages
+
+@app.route("/")
+def index():
+    return ''' <div>
+    Username:
+    <form>
+   <input>
+   <button>
+   Submit
+   </button>
+   </form>
+
+    </div>
+    
+    <div>
+        <div id="messages">
+
+        </div>
+            <form id="send-message">
+            
+                <input>
+                <button>
+                    Submit
+                </button>
+            </form>
+    </div>
+    
+    <script>
+    const form = document.querySelector("form");
+    const sendMessageForm = document.querySelector("#send-message");
+    const username = document.querySelector("input")
+    form.addEventListener("submit", event => {
+       console.log(username.value)
+       event.preventDefault()
+    })
+    sendMessageForm.addEventListener("submit", event => {
+       console.log(username.value)
+       event.preventDefault()
+       fetch("messages").then(response => {
+            return response.json()
+       }).then(data =>{
+        console.log(data)
+       })
+    })
+    setInterval(() => {
+    fetch("messages").then(response => {
+            return response.json()
+       }).then(data =>{
+        console.log(data)
+       })
+    },1000)
+    </script>'''
+
 
 app.run(host='0.0.0.0', port=81,debug=True)
